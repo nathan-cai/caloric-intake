d1f274606d9d71586da9eccd83910c9fe8a69269
Qien Nathan Cai <qien.nathan@gmail.com>
2022-02-19 11:00:35 -0500

    created UI for screen 2

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from classes import Game, InputBox, Button
+from classes import Game, InputBox, Button, Button_table
 from Website_request import Calories
 import pygame as pg
 from pygame.locals import *
@@ -13,7 +13,8 @@ def main():
     clock = pg.time.Clock()
     button1 = Button((180, 180, 180), 430, 215, 140, 35, 'Submit')
     input_box = InputBox((180, 180, 180), 200, 175, 600, 35)
-    back_button = Button((180, 180, 180), 430, 215, 140, 35, 'back')
+    back_button = Button((180, 180, 180), 25, 550, 140, 35, 'back')
+    run_once = True
 
     while True:
         clock.tick(60)
@@ -31,17 +32,25 @@ def main():
             input_box.draw(g.screen)
         # screen 2
         else:
+            if run_once:
+                c = Calories(input_box.search)
+                table1 = Button_table(c.results1, c.results2)
+                run_once = False
             for event in pg.event.get():
                 if event.type == pg.QUIT:
                     pg.quit()
                     exit()
                 back_button.is_clicked(event)
                 back_button.is_hover(event)
+                table1.is_hover(event)
+
             if back_button.clicked:
                 button1.clicked = False
                 input_box.submitted = False
                 back_button.clicked = False
+                run_once = True
             back_button.draw(g.screen)
+            table1.draw(g.screen)
         pg.display.update()
 
 
